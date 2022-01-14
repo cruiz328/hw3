15115e308e6c43ce7f05db67543d3d66853223a5
Christian Ruiz <lemmyruiz@Christians-MacBook-Pro.local>
2022-01-14 13:48:05 -0800

    finished code

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 function generatePassword() {
 console.log("Good job champ, the button works!!!!!")
-return "Password is cooking!"
+var passwordCriteria = ""
       // Length //
   let length = prompt('what is the length of password? 8-128 Characters available');
 
@@ -11,17 +11,40 @@ return "Password is cooking!"
   } else {
         // Uppercase //
     var addUppercase = window.confirm("Do you want uppercase? Yes or No?")
+    console.log(addUppercase)
+    if(addUppercase){
+      passwordCriteria+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      console.log(passwordCriteria)
+    }
         // Lowercase //
     var addLowercase = window.confirm("Do you want lowercase? Yes or No?")
+    console.log(addLowercase)
+    if(addLowercase){
+      passwordCriteria+="abcdefghijklmnopqrstuvwxyz"
+      console.log(passwordCriteria)
+    }
         // Numbers //
     var addNumbers = window.confirm("Do you want numbers? Yes or No?")
-    let numbers = prompt("how many? 1-5.")
-    if (parseInt(numbers) < 1 || parseInt(numbers) > 5) {
-      alert('invalid');
+    console.log(addNumbers)
+    if(addNumbers){
+      passwordCriteria+="0123456789"
+      console.log(passwordCriteria)
     }
         // Special Characters //
     var addSpecialCharacter = window.confirm("Do you want a special character? Yes or No?")
+    console.log(addSpecialCharacter)
+    if(addSpecialCharacter){
+      passwordCriteria+="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+      console.log(passwordCriteria)
+    }
 
+    var password = ""
+    for ( var i = 0; i < length; i++ ) {
+      password += passwordCriteria.charAt(Math.floor(Math.random() * 
+ passwordCriteria.length));
+      
+    }
+    return password
   }
 }
 
